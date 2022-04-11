f1c8113698393ba8d04272684eb1d661b53e89b7
relaxBear20 <64793398+relaxBear20@users.noreply.github.com>
2022-04-11 19:41:49 +0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://860c1873.au-syd.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
